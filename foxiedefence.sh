3220a5901783d4f0c4e387502cbca4542d89b427
Mehmet Demir <88983987+jackalkarlos@users.noreply.github.com>
2024-03-14 04:39:10 +0300

    Update and rename foxielastwatch.sh to foxiedefence.sh

--- foxiedefence.sh
+++ foxiedefence.sh
@@ -4,31 +4,11 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
+last_result=""
+last_user_count=""
+first_run=true
 
-function last_control() {
-  echo -e "${RED} Foxie is watching now!"
-  touch last_connections.txt
-  while true
-  do
-    new_line=$(last | head -n 1)
-    last_line=$(tail -n 1 last_connections.txt)
-    if [ "$new_line" != "$last_line" ]; then
-        echo -e "${RED}ALERT: NEW CONNECTION!"
-        echo -e "$new_line"
-        echo -e "$new_line" >> last_connections.txt
-        read -p "${GREEN}INPUT: Do you wanna block this connection? (Y/N): " kick_option
-        if [ "$kick_option" == "Y" ] || [ "$kick_option" == "y" ]; then
-            username=$(echo "$new_line" | awk '{print $1}')
-            sudo pkill -KILL -u "$username"
-            echo "User succesfully kicked."
-        fi
-    fi
-    sleep 1
-  done
-}
-
-printbanner(){
-echo -e "              ${GREEN}FOXIE LASTWATCH                "
+echo -e "              ${GREEN}FOXIE DEFENCE                "
 echo -e "       ⣿⣿⣿⣯⣉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⣉⣽⣿⣿⣿⡇"
 echo -e "       ⣿⣿⣿⣟⠿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡾⠟⣻⣿⣿⡟⡇"
 echo -e "       ⣿⣿⣿⣷⠀⠀⠹⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡾⠋⠀⢐⣾⣿⣿⠃⡇"
@@ -49,10 +29,29 @@ echo -e "       ⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⣿⡿⠛⠉⠀⠀�
 echo -e "       ⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇"
 echo -e "       ⣿⣿⣿⣯⣉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⣉⣽⣿⣿⣿⡇"
 
-echo -e "${RED} Printing recent 'Last' entries. "
-last
 
-}
+while true; do
+  yeni_result=$(last)
+  yeni_user_count=$(wc -l < /etc/passwd)
 
-printbanner
-last_control
+  if [ $? -eq 0 ]; then
+    if [ "$yeni_result" != "$last_result" ] && [ "$first_run" = false ]; then
+      echo -e "${RED}ALERT: NEW ENTRY ON LAST! ${NC}"
+      last
+    fi
+
+    if [ "$yeni_user_count" != "$last_user_count" ] && [ "$first_run" = false ]; then
+      echo -e "${RED}ALERT: NEW USER ON /ETC/PASSWD! ${NC}"
+      cat /etc/passwd
+    fi
+
+    last_result="$yeni_result"
+    last_user_count="$yeni_user_count"
+    first_run=false
+  else
+    echo -e "${RED}ALERT: SOMETHING WRONG WITH THIS SCRIPT. IT'S CLOSING."
+    exit 1
+  fi
+
+  sleep 3
+done
